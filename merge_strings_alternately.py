6a94147f3602ba33cc0c82162b432328160cb7a1
AMike <amike34542@gmail.com>
2024-12-25 19:51:12 +0900

    more solution

--- merge_strings_alternately.py
+++ merge_strings_alternately.py
@@ -1,3 +1,4 @@
+######################################################################################
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
         i = 0
@@ -10,7 +11,7 @@ class Solution:
             i +=1
             j +=1
         
-        # pointer 가 남아있으면
+        # pointer가 남아있으면
         #   남은 word의 pointer 오른쪽 부분을 전부 append
         if i < len(word1):
             result.append(word1[i:])
@@ -18,9 +19,31 @@ class Solution:
             result.append(word2[j:])
 
         return ''.join(result)
-    
-merge = Solution()
+######################################################################################
+class Solution2:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        # zip을 사용해서 동시에 join
+        merged = ''.join(a + b for a, b in zip(word1, word2))
+        # 남은 str을 오른쪽에 더하기
+        return merged + word1[len(word2):] + word2[len(word1):]
+######################################################################################
+class Solution3:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        result = []
+        for i in range(max(len(word1), len(word2))):
+            if i < len(word1):
+                result.append(word1[i])
+            if i < len(word2):
+                result.append(word2[i])
+        return ''.join(result)
+######################################################################################
 
-a = merge.mergeAlternately("abcd","pqr")
-print(a)
+# merge = Solution()
+# a = merge.mergeAlternately("abcd","pqr")
+# print(a)
 # apbqcrd
+
+merge = Solution2()
+a = merge.mergeAlternately("ab","pqr")
+print(a)
+# apbqr
