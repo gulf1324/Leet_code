66404bf15c0b72e1695720a85d0841e68ad0ff15
AMike <amike34542@gmail.com>
2025-01-04 18:33:52 +0900

    two pointers but left/right end each

---
+++ Solutions/Container With Most Water.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxArea(self, height: list[int]) -> int:
+        right = len(height) - 1
+        left = 0
+        area = (right-left)* min(height[right],height[left])
+        while left < right:
+            if height[right] < height[left]:
+                right -=1
+            else :
+                left += 1
+            if area < (right-left)* min(height[right],height[left]):
+                area = (right-left)* min(height[right],height[left])
+        return area
+                
+
+s = Solution()
+print(s.maxArea([1,8,6,1000,1000,4,8,3,7]))
