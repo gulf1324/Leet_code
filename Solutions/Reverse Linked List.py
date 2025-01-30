89b98fb24c026344d741b1cb50419e280772d7dc
AMike <amike34542@gmail.com>
2025-01-30 18:11:54 +0900

    linked list basic

---
+++ Solutions/Reverse Linked List.py
@@ -0,0 +1,39 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def print_list(head):
+    current = head
+    while current:
+        print(current.val, end=" -> ")
+        current = current.next
+    print("None")
+
+#####################################################################################################################################
+from typing import Optional
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        current = head
+        
+        while current is not None:
+            temp = current.next              # temp for not losing current.next(should assign this as current for next loop)  -----|
+                                             #                                                                                     |
+            current.next = prev              # cut link and link current.next to the previous node(1st case: None)                 |
+                                             #                                                                                     |
+            prev = current                   # assign "previous node" as "current node"                                            |
+            current = temp                   # assign "next node" as "current node"     <------------------------------------------|
+        
+        return prev
+#####################################################################################################################################
+head1 = ListNode(1)
+head1.next = ListNode(2)
+head1.next.next = ListNode(3)
+head1.next.next.next = ListNode(4)
+head1.next.next.next.next = ListNode(5)
+
+s = Solution()
+rev = s.reverseList(head1)
+print_list(rev)
