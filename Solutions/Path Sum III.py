3a4ceb4ff355d3b4e39b9d842609670cc27238dc
AMike <amike34542@gmail.com>
2025-02-07 04:27:13 +0900

    iterative/ recursive DFS 2

--- Solutions/Path Sum III.py
+++ Solutions/Path Sum III.py
@@ -76,6 +76,31 @@ class Solution:
 
         return count
 #################################################################################################################
+# recursive
+from collections import defaultdict
+class Solution:
+    def pathSum(self, root: TreeNode, targetSum: int) -> int:
+        prefix_sums = defaultdict(int)
+        prefix_sums[0] = 1  # Base case: A path sum that directly equals targetSum
+        
+        def dfs(node, curr_sum):
+            if not node:
+                return 0
+            
+            curr_sum += node.val
+            count = prefix_sums[curr_sum - targetSum]  # Count paths that sum to targetSum
+            ############################
+            prefix_sums[curr_sum] += 1 #
+            ############################
+            count += dfs(node.left, curr_sum)
+            count += dfs(node.right, curr_sum)
+            prefix_sums[curr_sum] -= 1
+            
+            return count
+        
+        return dfs(root, 0)
+#################################################################################################################
+
 # test case #1
 root = build_tree([10,5,-3,3,2,None,11,3,-2,None,1])
 s = Solution()
