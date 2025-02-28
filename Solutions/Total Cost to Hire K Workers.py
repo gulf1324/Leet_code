b98905222509c0cfb5bd356981f1558557154ee6
AMike <dksckdrjf123@naver.com>
2025-02-28 18:10:49 +0900

    attempt #3 => index out of range Index Error/ seperating heap_1, heap_2 might be a wrong approach

--- Solutions/Total Cost to Hire K Workers.py
+++ Solutions/Total Cost to Hire K Workers.py
@@ -6,15 +6,13 @@ class Solution:
         heap_2 = []
         res = 0
         
-        # len(costs) == 35 < 2 * candidates, resulting in half-half to heap_1 and heap_2
         for i in range(candidates):
             if any(costs):
                 heapq.heappush(heap_1, costs[i])
+                costs[i] = None
+            if any(costs):
                 heapq.heappush(heap_2, costs[-i-1])
-                costs[i], costs[-i-1] = None, None
-        # in last loop,
-        # heap_1 = [..., 25(exact middle element)]
-        # heap_2 = [..., 25(exact middle element)]
+                costs[-i-1] = None
         
         heap_1_i, heap_2_i = i+1, i+1
         while k > 0 :
@@ -37,15 +35,20 @@ class Solution:
         return res
 ########################################################################################################
 s = Solution()
-# print(s.totalCost(costs = [17,12,10,2,7,2,11,20,8], k = 3, candidates = 4))
-# # # >>> 11
+print(s.totalCost(costs = [17,12,10,2,7,2,11,20,8], k = 3, candidates = 4))
+# # >>> 11
 
-# print(s.totalCost(costs = [1,2,4,1], k = 3, candidates = 3))
-# # 4 
+print(s.totalCost(costs = [1,2,4,1], k = 3, candidates = 3))
+# 4 
 
-# print(s.totalCost(costs =[31,25,72,79,74,65,84,91,18,59,27,9,81,33,17,58], k =11, candidates =2))
+print(s.totalCost(costs =[31,25,72,79,74,65,84,91,18,59,27,9,81,33,17,58], k =11, candidates =2))
 # # 423 (but 430)
 
 print(s.totalCost(costs = [18,64,12,21,21,78,36,58,88,58,99,26,92,91,53,10,24,25,20,92,73,63,51,65,87,6,17,32,14,42,46,65,43,9,75],
                    k = 13, candidates = 23))
 # 223 (but 222)
+
+print(s.totalCost(costs = [69,10,63,24,1,71,55,46,4,61,78,21,85,52,83,77,42,21,73,2,80,99,98,89,55,94,63,50,43,62,14],
+                    k = 21, candidates = 31))
+
+# (out of index Index Error)
