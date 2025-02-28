d2a2a4bdcf431459aa70efb3a772d2bed23c69ee
AMike <dksckdrjf123@naver.com>
2025-02-28 19:19:23 +0900

    Correct => two pointers, two heaps

--- Solutions/Total Cost to Hire K Workers.py
+++ Solutions/Total Cost to Hire K Workers.py
@@ -1,51 +1,64 @@
 import heapq
 ########################################################################################################
+# Correct 
+# correct: 
+#  - Use 2 heaps(head, tail) / (no duplicates)
+#  - Keeps track of both heaps' indexes
+
+# fixed:
+#  - hiring process can happen only on one-side continuously. in this case, should check 
+#    whether the heap is empty.
+#  - left < right checks if there's any element left in ```costs```if so, should 
+#    be pushed to either left/right heap.
 class Solution:
     def totalCost(self, costs: list[int], k: int, candidates: int) -> int:
-        heap_1 = []
-        heap_2 = []
-        res = 0
-        
-        for i in range(candidates):
-            if any(costs):
-                heapq.heappush(heap_1, costs[i])
-                costs[i] = None
-            if any(costs):
-                heapq.heappush(heap_2, costs[-i-1])
-                costs[-i-1] = None
-        
-        heap_1_i, heap_2_i = i+1, i+1
-        while k > 0 :
-            for _ in range(i, len(costs)):
-                if heap_1[0] <= heap_2[0]:
-                    res += heapq.heappop(heap_1)
-                    if costs[heap_1_i]:
-                        heapq.heappush(heap_1, costs[heap_1_i])
-                        costs[heap_1_i] = None
-                        heap_1_i +=1
-                else:
-                    res += heapq.heappop(heap_2)
-                    if costs[-heap_2_i-1]:
-                        heapq.heappush(heap_2, costs[-heap_2_i-1])
-                        costs[-heap_2_i-1] =  None
-                        heap_2_i +=1 
-                k -= 1
-                if k == 0:
-                    return res
-        return res
+        # base case
+        if len(costs) <= 2*candidates:  
+            return sum(sorted(costs)[:k])
+
+        n = len(costs)
+        left_heap, right_heap = [], []
+        left_i, right_i = 0, n - 1
+        total_cost = 0
+
+        # (value, index)
+        for _ in range(candidates):
+            if left_i <= right_i:
+                heapq.heappush(left_heap, (costs[left_i], left_i))
+                left_i += 1
+            if left_i <= right_i:
+                heapq.heappush(right_heap, (costs[right_i], right_i))
+                right_i -= 1
+
+        # Hiring process
+        for _ in range(k):
+            if not right_heap or (left_heap and left_heap[0] <= right_heap[0]):
+                cost, idx = heapq.heappop(left_heap)
+                if left_i <= right_i:
+                    heapq.heappush(left_heap, (costs[left_i], left_i))
+                    left_i += 1
+            else:
+                cost, idx = heapq.heappop(right_heap)
+                if left_i <= right_i:
+                    heapq.heappush(right_heap, (costs[right_i], right_i))
+                    right_i -= 1
+            
+            total_cost += cost
+
+        return total_cost
 ########################################################################################################
 s = Solution()
 print(s.totalCost(costs = [17,12,10,2,7,2,11,20,8], k = 3, candidates = 4))
-# # >>> 11
+# # # # >>> 11
 
-print(s.totalCost(costs = [1,2,4,1], k = 3, candidates = 3))
-# 4 
+# print(s.totalCost(costs = [1,2,4,1], k = 3, candidates = 3))
+# # 4 
 
-print(s.totalCost(costs =[31,25,72,79,74,65,84,91,18,59,27,9,81,33,17,58], k =11, candidates =2))
+# print(s.totalCost(costs =[31,25,72,79,74,65,84,91,18,59,27,9,81,33,17,58], k =11, candidates =2))
 # # 423 (but 430)
 
-print(s.totalCost(costs = [18,64,12,21,21,78,36,58,88,58,99,26,92,91,53,10,24,25,20,92,73,63,51,65,87,6,17,32,14,42,46,65,43,9,75],
-                   k = 13, candidates = 23))
+# print(s.totalCost(costs = [18,64,12,21,21,78,36,58,88,58,99,26,92,91,53,10,24,25,20,92,73,63,51,65,87,6,17,32,14,42,46,65,43,9,75],
+#                    k = 13, candidates = 23))
 # 223 (but 222)
 
 print(s.totalCost(costs = [69,10,63,24,1,71,55,46,4,61,78,21,85,52,83,77,42,21,73,2,80,99,98,89,55,94,63,50,43,62,14],
